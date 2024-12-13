1a161eb2c7e7f08ae526d7d596619433aae8b459
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-13 17:02:26 +0300

    Created js file for menu page
    Added containers for different sections of menu

---
+++ src/menu.js
@@ -0,0 +1,13 @@
+const loadMenuPage = () => {
+  // create variable for div content
+  const content = document.getElementById("content");
+
+  // create containers for each section of menu: salads, main, deserts, beverages
+  const salads = document.createElement("div");
+
+  const mainCourse = document.createElement("div");
+
+  const deserts = document.createElement("div");
+
+  const beverages = document.createElement("div");
+};
